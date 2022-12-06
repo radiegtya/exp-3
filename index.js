02348500da034adbd78b6e02974a7be8d775d86c
Ega Wachid Radiegtya <radiegtya@gmail.com>
2022-12-06 10:03:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new microgenV3.MicrogenClient({
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World from tuesday!')
 })
 
 app.get("/products", async (req, res) => {
